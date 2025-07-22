7c6466f0e5cf3e4e11605a3b75cd5d963d1934cf
Renjith Raj <renjithraj2005@gmail.com>
2025-07-22 20:32:31 +0530

    GitHub pages deployment config removed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,10 +20,6 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
